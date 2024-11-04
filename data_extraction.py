e067aad62259ee3b8e9c7380fdd7a0b48faaeed1
rohanveit1 <rohan.veit@gmail.com>
2024-11-04 15:48:43 +0000

    cheeeeeck pleaaaase

---
+++ data_extraction.py
@@ -0,0 +1,9 @@
+import json
+import pandas as pd
+
+"""
+Utility methods for extracting meaningful dataframes from dataset JSON files
+"""
+
+def extract_top_countries() -> pd.DataFrame:
+    raise Exception('Not implemented yet')
