d5e798dce92ebd80a3c97a44df206a04ae4be0a1
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2024-12-05 15:34:32 +0000

    added implementation for the individual questions

--- cmdline.py
+++ cmdline.py
@@ -42,6 +42,7 @@ class CommandLineHandler:
         self.add_args()
 
         self.args = vars(self.parser.parse_args())
+        self.data_controller = data_controller.DataController()
 
     def add_args(self) -> None:
         self.parser.add_argument("-u", type=ArgTypes.user_uuid_type, required=False, help="The uuid to search for")
@@ -53,4 +54,54 @@ class CommandLineHandler:
         return self.args["t"] is not None
 
     def run(self):
-        print("running in cmd mode")
+
+        self.data_controller.change_file(self.args["f"])
+
+        match self.args["t"]:
+            case "2a":
+                self.q2a()
+            case "2b":
+                self.q2b()
+            case "3a":
+                self.q3a()
+            case "3b":
+                self.q3b()
+            case "4":
+                self.q4()
+            case "5d":
+                self.q5d()
+            case "6":
+                self.q6()
+            case "7":
+                self.q7()
+            case _:
+                raise Exception("Invalid Task")
+
+    def q2a(self):
+        df = self.data_controller.top_k_countries(10)
+        print(df)
+
+    def q2b(self):
+        df = self.data_controller.top_continents()
+        print(df)
+
+    def q3a(self):
+        df = self.data_controller.top_browsers(True)
+        print(df)
+
+    def q3b(self):
+        df = self.data_controller.top_browsers(False)
+        print(df)
+
+    def q4(self):
+        df = self.data_controller.top_k_readers(10)
+        print(df)
+
+    def q5d(self):
+        raise NotImplementedError
+
+    def q6(self):
+        raise NotImplementedError
+
+    def q7(self):
+        raise NotImplementedError
